17522d84df7513a781960f1d376355cf43fe6a3c
Kociński Piotr <44520613+astropio3000@users.noreply.github.com>
2021-07-08 13:59:25 +0200

    Entryes

---
+++ src/components/items/Entry.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="content">
+    <div class="image"><img :src="imagePath" :alt="'image of ' + title" /></div>
+    <div class="text">
+      <div class="title">
+        <h3>{{ title }}</h3>
+      </div>
+      <div class="description">
+        <p>{{ description }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["id"],
+  data: () => {
+    return {
+      imagePath: "images/loading.jpg",
+      title: "Loading...",
+      description:
+        "wait to load article from database, if you have poor network it can tak awhile",
+    };
+  },
+  created: function() {
+    // request for item with this.id
+  },
+};
+</script>
+
+<style></style>
--- src/components/items/Items.vue
+++ src/components/items/Items.vue
@@ -2,16 +2,25 @@
   <div class="main">
     <Menu></Menu>
     <Banner></Banner>
+
+    <Entry v-for="item in items" :key="item.id" :id="item.id"></Entry>
   </div>
 </template>
 
 <script>
 import Menu from "../main/Menu.vue";
 import Banner from "./Banner.vue";
+import Entry from "./Entry.vue";
 export default {
   components: {
     Menu,
     Banner,
+    Entry,
+  },
+  data: () => {
+    return {
+      items: [{ id: 1 }, { id: 2 }, { id: 3 }],
+    };
   },
 };
 </script>
